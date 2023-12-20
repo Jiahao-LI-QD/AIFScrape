a7c96d6c5ba059f05d8a82b3313276324aab2aa9
Quyy2023 <Eva.Qu2023@gmail.com>
2023-12-20 15:36:57 -0500

    Contract_Current

--- dbutilities/ia_db.py
+++ dbutilities/ia_db.py
@@ -59,6 +59,50 @@ DBCC CHECKIDENT ('Client_Current', reseed,0);
 def save_client(cursor, values):
     cursor.executemany("insert into Client_Current values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?, ?, ?, ?, ?, getdate())", values)
     cursor.commit()
+
+"""
+CREATE TABLE Contract_Current (
+    Contractinfo_id bigint PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    Applicant_last_name varchar(255) NOT NULL,
+    Applicant_first_name varchar(255) NOT NULL,
+    City varchar(255) NOT NULL,
+    Address varchar(255) NOT NULL,
+    Postal_code varchar(255) NOT NULL,
+    Province varchar(255) NOT NULL,
+    Country varchar(255) NOT NULL,
+    Address_validity varchar(255) NOT NULL,
+    Latest_change_of_address varchar(255) NOT NULL,
+    Main_phone varchar(255) NOT NULL,
+    Office_phone varchar(255),
+    Cell_phone varchar(255),
+    Applicant_email varchar(255) NOT NULL,
+    Applicant_age int Not NULL,
+    Birthday date NOT NULL,
+    Contract_number varchar(255) NOT NULL,
+    Product varchar(255) NOT NULL,
+    Type varchar(255) NOT NULL,
+    Contract_start_date varchar(255) NOT NULL,
+    Contract_market_value money NOT NULL,
+    Last_update_of_contract_market_value date NOT NULL,
+    PAC_amount money NOT NULL,
+    PAC_frequency int,
+    Day_of_the_PAC date NOT NULL,
+    Representative_name varchar(255) NOT NULL,
+    District varchar(255) NOT NULL,
+    U_S_ varchar(255) NOT NULL,
+    Representative_status varchar(255) NOT NULL,
+    Source varchar(255) NOT NULL, 
+    Snapshot_time datetime NOT NULL
+);
+
+drop table Contract_Current;
+
+select * from Contract_Current;
+
+delete from Contract_Current;
+
+DBCC CHECKIDENT ('Contract_Current', reseed,0);
+"""
 def save_fund(cursor, values):
     print("leyi branch")
 
