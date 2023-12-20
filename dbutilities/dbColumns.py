55e1258194f4de0a673670f207dd1f142995bae7
jiahao-LI-QD <jiahaoli2017@gmail.com>
2023-12-20 16:42:31 -0500

    12/20 dbcolumns.py & dbtemplate.py

--- dbutilities/dbColumns.py
+++ dbutilities/dbColumns.py
@@ -1,4 +1,4 @@
-high_interest_columns = ["Record_Date", "contract_number", "account_type", "investment_type", "rate", "balance"]
+saving_columns = ["Statement_Date", "Contract_number", "Account_type", "Investment_type", "Rate", "Balance"]
 
 client_columns = ["LastName", "FirstName", "Sex", "Language", "Birthday", "Address", "Address_line2", "City", "Province",
                   "Postal_Code", "Main_Phone", "Office_Phone", "Fax", "Cell_Phone", "Primary_Email", "Secondary_Email"]
