52685ca02cbb48ba61956d241570a3f805b4a5e2
jiahao-LI-QD <jiahaoli2017@gmail.com>
2023-12-20 17:44:01 -0500

    12/20 contract_tamplate lastest_update varchar to date

--- csv_generator.py
+++ csv_generator.py
@@ -1,9 +1,13 @@
 from dbutilities import dbColumns, dbtemplate
 from test import sample_csv
 import faker
+import random
+from datetime import date
 
 # sample_csv.random_data_generator(dbColumns.high_interest_columns,
 #                                  dbtemplate.hi_template,
 #                                  size=10001)
-
-print(dbtemplate.client_template)
+fake = faker.Faker()
+Sex = ["Male", "Female", "Other"]
+start_date = date(year=1950, month=1, day=1)
+print(dbtemplate.contract_template)
--- dbutilities/dbtemplate.py
+++ dbutilities/dbtemplate.py
@@ -3,13 +3,79 @@ import random
 import faker
 
 fake = faker.Faker()
-# Client_columns = ["LastName", "FirstName", "Sex", "Language", "Birthday", "Address", "Address_line2", "City", "Province",
-#                   "Postal_Code", "Main_Phone", "Office_Phone", "Fax", "Cell_Phone", "Primary_Email", "Secondary_Email"]
+
+# generate template for client
 Sex = ["Male", "Female", "Other"]
 start_date = date(year=1950, month=1, day=1)
-client_template = [fake.name().split(' ')[-1], fake.name().split(' ')[:-1], random.choice(Sex), "English",
-                   fake.date_between(start_date=start_date, end_date='today'), fake.address(), fake.city(), fake.province(),
-                   fake.postcode(), fake.phone_number(), fake.phone_number(), None, fake.phone_number(), fake.email(), fake.email()]
+client_template = [
+    fake.name().split(' ')[-1],  # last name
+    " ".join(fake.name().split(' ')[:-1]),  # first name
+    random.choice(Sex),  # Sex
+    "English",  # language
+    fake.date_between(start_date=start_date, end_date='today'),  # birthday
+    fake.address(),  # address
+    fake.city(),  # city
+    "ON",  # province
+    "XXX XXX",  # zip code
+    fake.phone_number(),  # Main_Phone
+    fake.phone_number(),  # Office_Phone
+    None,  # Fax
+    fake.phone_number(),  # Cell_Phone
+    fake.email(),  # Primary_Email
+    None  # Secondary_Email
+]
 
-hi_template = [date.today(), "contr" + str(random.randint(1, 10000)), "TFSA", "HI",
-               random.random() * 10, random.random() * 10000]
+
+class Contract_number:
+    value = 0
+
+    def new_contract_number(self):
+        self.value += 1
+        return self.value
+
+
+# generate template for contract
+cn = Contract_number()
+Latest_change = date(year=2020, month=1, day=1)
+account_type = ["TFSA", "Non-registered"]
+contract_template = [
+    fake.name().split(' ')[-1],  # Applicant_last_name varchar(255) NOT NULL,
+    " ".join(fake.name().split(' ')[:-1]),  # Applicant_first_name varchar(255) NOT NULL,
+    fake.city(),  # City varchar(255) NOT NULL,
+    fake.address(),  # Address varchar(255) NOT NULL,
+    "XXX XXX",  # Postal_code varchar(255) NOT NULL,
+    "ON",  # Province varchar(255) NOT NULL,
+    fake.country(),  # Country varchar(255) NOT NULL,
+    "Compliant",  # Address_validity varchar(255) NOT NULL,
+    fake.date_between(start_date=Latest_change, end_date='today'),  # Latest_change_of_address varchar(255) NOT NULL,
+    fake.phone_number(),  # Main_phone varchar(255) NOT NULL,
+    None,  # Office_phone varchar(255),
+    None,  # Cell_phone varchar(255),
+    fake.email(),  # Applicant_email varchar(255) NOT NULL,
+    random.randint(18, 80),  # Applicant_age int Not NULL,
+    fake.date_between(start_date=start_date, end_date='today'),  # Birthday date NOT NULL,
+    1000000000 + cn.new_contract_number(),  # Contract_number varchar(255) NOT NULL,
+    "IAG SRP",  # Product varchar(255) NOT NULL,
+    random.choice(account_type),  # Type varchar(255) NOT NULL,
+    fake.date_between(start_date=Latest_change, end_date='today'),  # Contract_start_date varchar(255) NOT NULL,
+    random.random() * 100000,  # Contract_market_value money NOT NULL,
+    fake.date_between(start_date=Latest_change, end_date='today'),  # Last_update_of_contract_market_value date NOT NULL
+    random.random() * 100000,  # PAC_amount money NOT NULL,
+    None,  # PAC_frequency int,
+    fake.date_between(start_date=Latest_change, end_date='today'),  # Day_of_the_PAC date NOT NULL,
+    fake.name(),  # Representative_name varchar(255) NOT NULL,
+    "XXX",  # District varchar(255) NOT NULL,
+    "XXX",  # U_S_ varchar(255) NOT NULL,
+    "Active",  # Representative_status varchar(255) NOT NULL,
+    "IFAST"  # Source varchar(255) NOT NULL,
+]
+
+# generate template for saving
+saving_template = [
+    fake.date_between(start_date=start_date, end_date='today'),  # Statement_Date
+    None,  # Contract_number
+    "TFSA",  # Account_type
+    "HI",  # Investment_type
+    random.random() * 10,  # Rate
+    random.random() * 10000  # balance
+]
