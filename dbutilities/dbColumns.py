fa67302b479dbf599df979bc41e7d03a429584d3
Forrest <forrest0407@gmail.com>
2023-12-20 16:55:42 -0500

    Added DB table columns.

--- dbutilities/dbColumns.py
+++ dbutilities/dbColumns.py
@@ -3,8 +3,12 @@ saving_columns = ["Statement_Date", "Contract_number", "Account_type", "Investme
 client_columns = ["LastName", "FirstName", "Sex", "Language", "Birthday", "Address", "Address_line2", "City", "Province",
                   "Postal_Code", "Main_Phone", "Office_Phone", "Fax", "Cell_Phone", "Primary_Email", "Secondary_Email"]
 
+transaction_columns = ["Contract_number", "Transaction_Date", "Transaction_type", "Fundcode", "Gross_amount", "Units",
+                       "Unit_value"]
+
+fund_columns = ["Statement_Date", "Contract_number", "Account_type", "Investment_type", "Category", "Fund_name", "Units",
+                "Unit_value", "Value", "ACB"]
+
 participant_columns = ["Contract_number", "Role", "Name", "Birthday"]
 
 beneficiary_columns = ["Contract_number", "Name", "Allocation", "Relationship", "Class"]
-
-hihihihi
