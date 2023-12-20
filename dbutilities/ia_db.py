9c0bb7373054a881e656aaa2201dd8a2bc563b7c
Leyi <christinac0617@gmail.com>
2023-12-20 15:17:12 -0500

    FUND_current creation

--- dbutilities/ia_db.py
+++ dbutilities/ia_db.py
@@ -82,12 +82,42 @@ select * from Transaction_Current;
 
 delete from Transaction_Current;
 
-DBCC CHECKIDENT ('Client_Current', reseed,0);
+DBCC CHECKIDENT ('Transaction_Current', reseed,0);
 """
 def save_transaction(cursor, values):
     cursor.executemany("insert into Transaction_Current values (?, ?, ?, ?, ?, ?, ?, getdate())", values)
     cursor.commit()
+#CURRENT FUND TABLE CREATION
 
+"""
+CREATE TABLE Fund_Current (
+    Fund_id BIGINT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    Statement_Date DATE NOT NULL,
+    Contract_number varchar(255) NOT NULL,
+    Account_type varchar(255) NOT NULL,
+    Investment_type varchar(255) NOT NULL,
+    Category varchar(255) NOT NULL,
+    Fund_name varchar(255) NOT NULL,
+    Units real NOT NULL,
+    Unit_value real NOT NULL,
+    Value money NOT NULL,
+    ACB money,
+    Snapshot_time datetime NOT NULL,
+    CONSTRAINT FK_Fund_Contract_number FOREIGN KEY (Contract_number) REFERENCES Contract_Current (Contract_number)
+);
+
+drop table Fund_Current;
+
+select * from Fund_Current;
+
+delete from Fund_Current;
+
+DBCC CHECKIDENT ('Fund_Current', reseed,0);
+"""
+
+def save_fund(cursor, values):
+    cursor.executemany("insert into Fund_Current values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, getdate())", values)
+    cursor.commit()
 def save_fund(cursor, values):
     print("leyi branch")
 
