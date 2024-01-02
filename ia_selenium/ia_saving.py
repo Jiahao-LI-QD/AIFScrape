d1bbc57ef77db9608e378b3fd6bc0f909d336eec
Leyi <christinac0617@gmail.com>
2024-01-02 18:01:14 -0500

    ia high interest saving

--- ia_selenium/ia_saving.py
+++ ia_selenium/ia_saving.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from ia_selenium import keys
+from datetime import datetime
 
 wd = webdriver.Chrome()
 
@@ -33,13 +34,18 @@ wd.find_element(By.XPATH, '//*[@id="btnSearch"]').click()
 wd.find_element(By.XPATH,'//*[@id="Placements"]').click()
 type = wd.find_element(By.XPATH,'//*[@id="content"]/div[4]/div[1]/div/div[1]').text
 
-Statement_Date = wd.find_element(By.XPATH, '//*[@id="content"]/div[3]').text
-Contract_number = wd.find_element(By.XPATH, '//*[@id="content"]/div[1]/div[1]/div/span').text
-Account_type = wd.find_element(By.XPATH, '//*[@id="content"]/div[1]/div[1]/div/span').text
+text = wd.find_element(By.XPATH, '//*[@id="content"]/div[1]/div[1]/div/span').text
+date_text = wd.find_element(By.XPATH, '//*[@id="content"]/div[3]').text.split(' ', 2)[2]
+date_obj = datetime.strptime(date_text, '%B %d, %Y')
+formatted_date = date_obj.strftime('%Y-%m-%d')
+Contract_number = text.split(' - ')[1]
+Account_type = text.split(' - ')[2]
 Investment_type = wd.find_element(By.XPATH, '//*[@id="content"]/div[4]/div[1]/div/div[1]').text
 Rate = wd.find_element(By.XPATH, '//*[@id="content"]/div[4]/div[2]/table/tbody/tr[2]/td[1]').text
 Balance = wd.find_element(By.XPATH, '//*[@id="content"]/div[4]/div[2]/table/tbody/tr[2]/td[2]').text
-Category =wd.find_elements(By.XPATH,)
+
+print(formatted_date, Contract_number, Account_type, Investment_type, Rate, Balance)
+# print(Statement_Date, Contract_number, Account_type, Investment_type, Rate, Balance)
 # #Fund_name
 # Units =
 # Unit_value =
