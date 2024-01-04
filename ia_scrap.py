90d48ca70dc4ef9afea67ca2d68e640c3b126fbc
jiahao-LI-QD <jiahaoli2017@gmail.com>
2024-01-04 17:44:40 -0500

    24/1/4 ia_scrap with multiple

--- ia_scrap.py
+++ ia_scrap.py
@@ -1,4 +1,5 @@
 import os.path
+import sys
 import time
 
 from selenium import webdriver
@@ -13,12 +14,58 @@ from ia_selenium import keys
 from selenium.webdriver.support import expected_conditions as EC
 from dbutilities import connection
 
+
+## control unit
+control_unit = 1
+start_contract = None
+if len(sys.argv) > 1:
+    control_unit = sys.argv[1]
+if len(sys.argv) > 2:
+    start_contract = sys.argv[2]
+if control_unit not in [1, 2, 3]:
+    print(f"The control model is not supported")
+
+if control_unit == 1:
+    print("Task: Scrape Investments")
+elif control_unit == 2:
+    print("Task: Transactions")
+else:
+    print("Task: Clients Information")
+print("======================")
+
+# alternative
+# control_variable = input()
+# control_unit = 1 if control_variable == "2" else 0
+#
+# print("If you want to continue from a specific contract number")
+# print("If yes, please input a valid contract number")
+# start_number = input()
+# while not start_number.isnumeric():
+#     print("This is not a valid contract number")
+#     start_number = input()
+
 # required parameters for app
 try:
     parameters = keys.ia_account()
 except Exception as e:
     print(e)
     exit()
+
+
+contracts = pd.read_excel(os.path.join(parameters['csv_path'], parameters['contracts']))
+contracts.columns = dbColumns.contract_columns
+start_index = 2
+if start_contract is not None:
+    indexes = contracts[contracts.Contract_number == start_contract].index
+    if len(indexes) == 0:
+        print("The contract number doesn't exist")
+        exit()
+    start_index = contracts[contracts.Contract_number == start_contract].index.values[0]
+    print(f"Starting from contract number {start_contract}")
+else:
+    print("Scraping all contract from beginning")
+print("======================")
+
 
 # start web driver
 wd = webdriver.Chrome()
@@ -36,19 +83,20 @@ wait.until(
     EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[2]/a[1]"))
 ).click()
 
-contracts = pd.read_excel(os.path.join(parameters['csv_path'], parameters['contracts']))
-contracts.columns = dbColumns.contract_columns
 
 # create pointers
-client = pd.DataFrame(columns=dbColumns.client_columns)
-saving = pd.DataFrame(columns=dbColumns.saving_columns)
-beneficiary = pd.DataFrame(columns=dbColumns.beneficiary_columns)
-participant = pd.DataFrame(columns=dbColumns.participant_columns)
-transaction = pd.DataFrame(columns=dbColumns.transaction_columns)
-fund = pd.DataFrame(columns=dbColumns.fund_columns)
+if control_unit == 1:
+    saving = pd.DataFrame(columns=dbColumns.saving_columns)
+    fund = pd.DataFrame(columns=dbColumns.fund_columns)
+elif control_unit == 2:
+    transaction = pd.DataFrame(columns=dbColumns.transaction_columns)
+else:
+    beneficiary = pd.DataFrame(columns=dbColumns.beneficiary_columns)
+    participant = pd.DataFrame(columns=dbColumns.participant_columns)
+    client = pd.DataFrame(columns=dbColumns.client_columns)
 
 
-for index, row in contracts.iloc[2:].iterrows():
+for index, row in contracts.iloc[start_index:].iterrows():
     print(f"scrapping for contract number {row['Contract_number']}")
 
     # TODO: Control Unit
@@ -60,25 +108,28 @@ for index, row in contracts.iloc[2:].iterrows():
         wd.find_element(By.XPATH, '//*[@id="ContractNumber"]').send_keys(row['Contract_number'])
 
         wd.find_element(By.XPATH, '//*[@id="btnSearch"]').click()
-
-        ia_investment.scrape_investment(wd, fund, saving)
-
-        ia_transactions.scrape_transaction(wd, transaction, row['Contract_start_date'])
-
-        ia_client.scrape(wd, client, beneficiary, participant)
+        if control_unit == 1:
+            ia_investment.scrape_investment(wd, fund, saving)
+        elif control_unit == 2:
+            ia_transactions.scrape_transaction(wd, transaction, row['Contract_start_date'])
+        else:
+            ia_client.scrape(wd, client, beneficiary, participant)
     except Exception as e:
         print(e)
         print(f"something happens when account number is {row['Contract_number']}")
 
 print("scrape complete")
 print("=========================")
-fund.to_csv(os.path.join(parameters['csv_path'],'funds.csv'))
-saving.to_csv(os.path.join(parameters['csv_path'],'savings.csv'))
-transaction.to_csv(os.path.join(parameters['csv_path'],'transactions.csv'))
-client.to_csv(os.path.join(parameters['csv_path'],'clients.csv'))
-beneficiary.to_csv(os.path.join(parameters['csv_path'],'beneficiaries.csv'))
-participant.to_csv(os.path.join(parameters['csv_path'],'participants.csv'))
-contracts.iloc[2:].to_csv(os.path.join(parameters['csv_path'],'contracts.csv'))
+if control_unit == 1:
+    fund.to_csv(os.path.join(parameters['csv_path'],'funds.csv'))
+    saving.to_csv(os.path.join(parameters['csv_path'],'savings.csv'))
+elif control_unit == 2:
+    transaction.to_csv(os.path.join(parameters['csv_path'],'transactions.csv'))
+else:
+    client.to_csv(os.path.join(parameters['csv_path'], 'clients.csv'))
+    beneficiary.to_csv(os.path.join(parameters['csv_path'], 'beneficiaries.csv'))
+    participant.to_csv(os.path.join(parameters['csv_path'], 'participants.csv'))
+contracts.iloc[start_index:].to_csv(os.path.join(parameters['csv_path'],'contracts.csv'))
 print("Saving to CSVS")
 print("=========================")
 
@@ -90,20 +141,22 @@ except Exception as e:
     print("Database connection failed!")
 else:
     print("Database connection successful!")
-    size = 1000
-    save_data_into_db(cursor, os.path.join(parameters['csv_path'],'clients.csv'), ia_db.save_client_history, 1000)
-
-    save_data_into_db(cursor, os.path.join(parameters['csv_path'],'participants.csv'), ia_db.save_participant, 1000)
-
-    save_data_into_db(cursor, os.path.join(parameters['csv_path'],'beneficiaries.csv'), ia_db.save_beneficiary, 1000)
-
-    save_data_into_db(cursor, os.path.join(parameters['csv_path'],'savings.csv'), ia_db.save_saving_history, 1000)
-
-    save_data_into_db(cursor, os.path.join(parameters['csv_path'],'funds.csv'), ia_db.save_fund_history, 1000)
-
-    save_data_into_db(cursor, os.path.join(parameters['csv_path'],'transactions.csv'), ia_db.save_transaction, 1000)
-
-    save_data_into_db(cursor, os.path.join(parameters['csv_path'],'contracts.csv'), ia_db.save_contract, 1000)
+    batch_size = 1000
+    save_data_into_db(cursor, os.path.join(parameters['csv_path'], 'contracts.csv'), ia_db.save_contract_history,
+                      batch_size)
+    if control_unit == 1:
+        save_data_into_db(cursor, os.path.join(parameters['csv_path'],'savings.csv'), ia_db.save_saving_history, batch_size)
+        save_data_into_db(cursor, os.path.join(parameters['csv_path'],'funds.csv'), ia_db.save_fund_history, batch_size)
+    elif control_unit == 2:
+        save_data_into_db(cursor, os.path.join(parameters['csv_path'], 'transactions.csv'),
+                          ia_db.save_transaction_history, batch_size)
+    else:
+        save_data_into_db(cursor, os.path.join(parameters['csv_path'], 'participants.csv'),
+                          ia_db.save_participant_history, batch_size)
+        save_data_into_db(cursor, os.path.join(parameters['csv_path'], 'beneficiaries.csv'),
+                          ia_db.save_beneficiary_history, batch_size)
+        save_data_into_db(cursor, os.path.join(parameters['csv_path'], 'clients.csv'), ia_db.save_client_history,
+                          batch_size)
 
 print("Saving to Databases")
 print("=========================")
