1a9b8e78f4928572bdf30e96b16ca8cc6103cd7b
Leyi <christinac0617@gmail.com>
2024-01-04 12:55:18 -0500

    created ia_selector dictionary replace xpath to variables

--- ia_selenium/ia_saving.py
+++ ia_selenium/ia_saving.py
@@ -1,25 +1,26 @@
 from datetime import datetime
 from selenium.webdriver.common.by import By
-
+from ia_selenium import ia_selectors
 
 def scrape(wd, saving):
-    wd.find_element(By.XPATH, '//*[@id="Placements"]').click()
+    paths = ia_selectors.saving_paths()
+    wd.find_element(By.XPATH, paths['investment_bottom']).click()
 
     #statement date
-    date_text = wd.find_element(By.XPATH, '//*[@id="content"]/div[3]').text.split(' ', 2)[2]
+    date_text = wd.find_element(By.XPATH, paths['date_text']).text.split(' ', 2)[2]
     date_obj = datetime.strptime(date_text, '%B %d, %Y')
     formatted_date = date_obj.strftime('%Y-%m-%d')
 
     #contract number and account type
-    text = wd.find_element(By.XPATH, '//*[@id="content"]/div[1]/div[1]/div/span').text
+    text = wd.find_element(By.XPATH, paths['contract_number_account_type']).text
     contract_number = text.split(' - ')[1]
     account_type = text.split(' - ')[2]
 
     #investment type
-    investment_type = wd.find_element(By.XPATH, '//*[@id="content"]/div[4]/div[1]/div/div[1]').text
+    investment_type = wd.find_element(By.XPATH, paths['investment_type']).text
 
     #rate and balance
-    rate = wd.find_element(By.XPATH, '//*[@id="content"]/div[4]/div[2]/table/tbody/tr[2]/td[1]').text
-    balance = wd.find_element(By.XPATH, '//*[@id="content"]/div[4]/div[2]/table/tbody/tr[2]/td[2]').text
+    rate = wd.find_element(By.XPATH, paths['rate']).text
+    balance = wd.find_element(By.XPATH, paths['balance']).text
 
     saving.loc[len(saving)] = [formatted_date, contract_number, account_type, investment_type, rate, balance]
