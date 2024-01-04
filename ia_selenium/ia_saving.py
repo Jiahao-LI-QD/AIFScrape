c9ab2956dda6bf95cd4347bf420450874706f5d3
Leyi <christinac0617@gmail.com>
2024-01-04 11:56:49 -0500

    added comment in ia_saving deleted useless lines

--- ia_selenium/ia_saving.py
+++ ia_selenium/ia_saving.py
@@ -4,17 +4,22 @@ from selenium.webdriver.common.by import By
 
 def scrape(wd, saving):
     wd.find_element(By.XPATH, '//*[@id="Placements"]').click()
-    type = wd.find_element(By.XPATH, '//*[@id="content"]/div[4]/div[1]/div/div[1]').text
 
-    text = wd.find_element(By.XPATH, '//*[@id="content"]/div[1]/div[1]/div/span').text
+    #statement date
     date_text = wd.find_element(By.XPATH, '//*[@id="content"]/div[3]').text.split(' ', 2)[2]
     date_obj = datetime.strptime(date_text, '%B %d, %Y')
     formatted_date = date_obj.strftime('%Y-%m-%d')
-    Contract_number = text.split(' - ')[1]
-    Account_type = text.split(' - ')[2]
-    Investment_type = wd.find_element(By.XPATH, '//*[@id="content"]/div[4]/div[1]/div/div[1]').text
-    Rate = wd.find_element(By.XPATH, '//*[@id="content"]/div[4]/div[2]/table/tbody/tr[2]/td[1]').text
-    Balance = wd.find_element(By.XPATH, '//*[@id="content"]/div[4]/div[2]/table/tbody/tr[2]/td[2]').text
 
+    #contract number and account type
+    text = wd.find_element(By.XPATH, '//*[@id="content"]/div[1]/div[1]/div/span').text
+    contract_number = text.split(' - ')[1]
+    account_type = text.split(' - ')[2]
 
-    saving.loc[len(saving)] = [formatted_date, Contract_number, Account_type, Investment_type, Rate, Balance]
+    #investment type
+    investment_type = wd.find_element(By.XPATH, '//*[@id="content"]/div[4]/div[1]/div/div[1]').text
+
+    #rate and balance
+    rate = wd.find_element(By.XPATH, '//*[@id="content"]/div[4]/div[2]/table/tbody/tr[2]/td[1]').text
+    balance = wd.find_element(By.XPATH, '//*[@id="content"]/div[4]/div[2]/table/tbody/tr[2]/td[2]').text
+
+    saving.loc[len(saving)] = [formatted_date, contract_number, account_type, investment_type, rate, balance]
