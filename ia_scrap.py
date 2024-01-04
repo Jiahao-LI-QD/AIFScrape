0f1e7db8afed7380e2f40d5320d7d0f7112fff54
Leyi <christinac0617@gmail.com>
2024-01-04 16:43:25 -0500

    added scrape and login xpath into ia_selector

--- ia_scrap.py
+++ ia_scrap.py
@@ -12,6 +12,7 @@ import pandas as pd
 from ia_selenium import keys
 from selenium.webdriver.support import expected_conditions as EC
 from dbutilities import connection
+from ia_selenium import ia_selectors
 
 # required parameters for app
 try:
@@ -28,12 +29,13 @@ wd.implicitly_wait(15)
 wd.get(parameters['web_url'])
 
 ia_login.login(wd, parameters['username'], parameters['password'])
+paths = ia_selectors.scrape_paths()
 # accept cookie
 # accept cookie
 time.sleep(1)
 wait = WebDriverWait(wd, 10)  # seconds want to wait
 wait.until(
-    EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div[2]/a[1]"))
+    EC.element_to_be_clickable((By.XPATH, paths['cookie_button']))
 ).click()
 
 contracts = pd.read_excel(os.path.join(parameters['csv_path'], parameters['contracts']))
@@ -50,16 +52,15 @@ fund = pd.DataFrame(columns=dbColumns.fund_columns)
 
 for index, row in contracts.iloc[2:].iterrows():
     print(f"scrapping for contract number {row['Contract_number']}")
-
     # TODO: Control Unit
     try:
-        wd.find_element(By.XPATH, '//*[@id="mnMesClients"]/a').click()
+        wd.find_element(By.XPATH, paths['myclient_button']).click()
 
-        wd.find_element(By.XPATH, '//*[@id="ContractNumber"]').clear()
+        wd.find_element(By.XPATH, paths['contract_number_input']).clear()
 
-        wd.find_element(By.XPATH, '//*[@id="ContractNumber"]').send_keys(row['Contract_number'])
+        wd.find_element(By.XPATH, paths['contract_number_input']).send_keys(row['Contract_number'])
 
-        wd.find_element(By.XPATH, '//*[@id="btnSearch"]').click()
+        wd.find_element(By.XPATH, paths['search_button']).click()
 
         ia_investment.scrape_investment(wd, fund, saving)
 
@@ -72,39 +73,39 @@ for index, row in contracts.iloc[2:].iterrows():
 
 print("scrape complete")
 print("=========================")
-fund.to_csv(os.path.join(parameters['csv_path'],'funds.csv'))
-saving.to_csv(os.path.join(parameters['csv_path'],'savings.csv'))
-transaction.to_csv(os.path.join(parameters['csv_path'],'transactions.csv'))
-client.to_csv(os.path.join(parameters['csv_path'],'clients.csv'))
-beneficiary.to_csv(os.path.join(parameters['csv_path'],'beneficiaries.csv'))
-participant.to_csv(os.path.join(parameters['csv_path'],'participants.csv'))
-contracts.iloc[2:].to_csv(os.path.join(parameters['csv_path'],'contracts.csv'))
-print("Saving to CSVS")
-print("=========================")
-
-
-try:
-    cursor = connection.connect_db().cursor()
-except Exception as e:
-    print(e)
-    print("Database connection failed!")
-else:
-    print("Database connection successful!")
-    size = 1000
-    save_data_into_db(cursor, os.path.join(parameters['csv_path'],'clients.csv'), ia_db.save_client_history, 1000)
-
-    save_data_into_db(cursor, os.path.join(parameters['csv_path'],'participants.csv'), ia_db.save_participant, 1000)
-
-    save_data_into_db(cursor, os.path.join(parameters['csv_path'],'beneficiaries.csv'), ia_db.save_beneficiary, 1000)
-
-    save_data_into_db(cursor, os.path.join(parameters['csv_path'],'savings.csv'), ia_db.save_saving_history, 1000)
-
-    save_data_into_db(cursor, os.path.join(parameters['csv_path'],'funds.csv'), ia_db.save_fund_history, 1000)
-
-    save_data_into_db(cursor, os.path.join(parameters['csv_path'],'transactions.csv'), ia_db.save_transaction, 1000)
-
-    save_data_into_db(cursor, os.path.join(parameters['csv_path'],'contracts.csv'), ia_db.save_contract, 1000)
-
-print("Saving to Databases")
-print("=========================")
+# fund.to_csv(os.path.join(parameters['csv_path'],'funds.csv'))
+# saving.to_csv(os.path.join(parameters['csv_path'],'savings.csv'))
+# transaction.to_csv(os.path.join(parameters['csv_path'],'transactions.csv'))
+# client.to_csv(os.path.join(parameters['csv_path'],'clients.csv'))
+# beneficiary.to_csv(os.path.join(parameters['csv_path'],'beneficiaries.csv'))
+# participant.to_csv(os.path.join(parameters['csv_path'],'participants.csv'))
+# contracts.iloc[2:].to_csv(os.path.join(parameters['csv_path'],'contracts.csv'))
+# print("Saving to CSVS")
+# print("=========================")
+#
+#
+# try:
+#     cursor = connection.connect_db().cursor()
+# except Exception as e:
+#     print(e)
+#     print("Database connection failed!")
+# else:
+#     print("Database connection successful!")
+#     size = 1000
+#     save_data_into_db(cursor, os.path.join(parameters['csv_path'],'clients.csv'), ia_db.save_client_history, 1000)
+#
+#     save_data_into_db(cursor, os.path.join(parameters['csv_path'],'participants.csv'), ia_db.save_participant, 1000)
+#
+#     save_data_into_db(cursor, os.path.join(parameters['csv_path'],'beneficiaries.csv'), ia_db.save_beneficiary, 1000)
+#
+#     save_data_into_db(cursor, os.path.join(parameters['csv_path'],'savings.csv'), ia_db.save_saving_history, 1000)
+#
+#     save_data_into_db(cursor, os.path.join(parameters['csv_path'],'funds.csv'), ia_db.save_fund_history, 1000)
+#
+#     save_data_into_db(cursor, os.path.join(parameters['csv_path'],'transactions.csv'), ia_db.save_transaction, 1000)
+#
+#     save_data_into_db(cursor, os.path.join(parameters['csv_path'],'contracts.csv'), ia_db.save_contract, 1000)
+#
+# print("Saving to Databases")
+# print("=========================")
 
--- ia_selenium/ia_selectors.py
+++ ia_selenium/ia_selectors.py
@@ -16,3 +16,23 @@ def fund_paths():
             'table_rows':  ".//*"
         }
     }
+
+
+def login_paths():
+    return {
+        'sign_in_button': '//*[@id="eeCleanLoader"]/div/div[1]/div/div/div/div/div[2]/div[1]/div/div/div[1]/a',
+        'username': '//*[@id="idp-discovery-username"]',
+        'cookie_button': '/html/body/div[2]/div[2]/a[1]',
+        'submit_username': '//*[@id="idp-discovery-submit"]',
+        'password': '//*[@id="okta-signin-password"]',
+        'submit_password': '//*[@id="okta-signin-submit"]'
+    }
+
+
+def scrape_paths():
+    return {
+        'myclient_button': '//*[@id="mnMesClients"]/a',
+        'contract_number_input': '//*[@id="ContractNumber"]',
+        'search_button': '//*[@id="btnSearch"]',
+        'cookie_button': '/html/body/div[2]/div[2]/a[1]'
+    }
