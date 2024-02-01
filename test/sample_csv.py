5f4284623a0c8f9ccf64a9dd705b888c78b9f6e8
jiahao-LI-QD <jiahaoli2017@gmail.com>
2024-02-01 17:17:54 -0500

    24/2/1 Remove Test folder from project

--- test/sample_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def random_data_generator(record_columns, template, size):
-    samples = pd.DataFrame(columns=record_columns)
-    data = template(size)
-    for i in range(size):
-        samples.loc[len(samples)] = data[i]
-    return samples
