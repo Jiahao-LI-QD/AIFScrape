f062f1b4fad32c81022c7bcefa632c9a2fdcdaaa
Forrest <forrest0407@gmail.com>
2024-02-01 17:10:57 -0500

    optimized transactions

--- ia_selenium/ia_selectors.py
+++ ia_selenium/ia_selectors.py
@@ -59,7 +59,6 @@ def transactions_path():
         'has_transaction': '//*[@id="TransactionsTrouveesDiv"]/div[3]/table/tbody/tr/td',
         'next_page': '//*[@id="TransactionsTrouveesDiv"]/div[3]/table/tfoot/tr/td/a/span',
         'CSS_next_page': '#TransactionsTrouveesDiv > div:nth-child(6) > table > tfoot > tr > td > a.suivant > span'
-        #bouton-avis > div
     }
 
 
