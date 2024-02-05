6a57c0e087fcbf30f5a9e5d25d31ca4355eac271
Forrest <forrest0407@gmail.com>
2024-02-05 18:32:53 -0500

    added CL functions and py files

--- cl_selenium/cl_selectors.py
+++ cl_selenium/cl_selectors.py
@@ -13,3 +13,17 @@ def traverse_paths():
         'search_button': '/html/body/div[3]/div/header/div[2]/div/div[2]/div/div[1]/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/ul/li[2]',
         'holdings': '//*[@id="2__item"]',
     }
+
+
+def transaction_paths():
+    return {
+        'Transactions_button': '//*[@id="4__item"]',
+        'issue_date': '//*[@id="1"]/div[2]/div[2]/div/div[2]/span',
+        'start_date': 'transactionStartDay',
+        'start_date_full': '<input class="slds-input" type="text" id="input-875" part="input" name="transactionStartDay" placeholder="MMM DD, YYYY" autocomplete="off" aria-describedby="range-message-875">',
+        'end_date': 'transactionEndDay',
+        'end_date_full': '<input class="slds-input" type="text" id="input-878" part="input" name="transactionEndDay" placeholder="MMM DD, YYYY" autocomplete="off" aria-describedby="range-message-878">',
+        'date_apply': '//*[@id="4"]/article/div[2]/div[1]/div/span[2]/button[1]',
+        'contract_number': '/html/body/div[3]/div/div[1]/div/div/div/div/div[3]/div/div/div[1]/header/div[2]/div/div[1]/div[2]/h1/div[2]/span',
+        'table_body': '//*[@id="4"]/article/div[2]/div[2]/table/tbody'
+    }
--- test/cl_test.py
+++ test/cl_test.py
@@ -1,8 +1,9 @@
 from time import sleep
+import pandas as pd
 
 from selenium.webdriver.common.by import By
 
-from cl_selenium import cl_scrap, cl_selectors
+from cl_selenium import cl_scrap, cl_selectors, cl_transactions
 
 # save cl_conf as parameters
 parameters = cl_scrap.cl_account()
@@ -16,15 +17,23 @@ wd.get(parameters['web_url'])
 cl_scrap.login(wd, parameters['username'], parameters['password'])
 
 # Going to an account for testing
-paths = cl_selectors.traverse_paths()
-wd.find_element(By.XPATH, paths['search_field']).send_keys(410351753)
+traverse_paths = cl_selectors.traverse_paths()
+wd.find_element(By.XPATH, traverse_paths['search_field']).send_keys(410351753)
 sleep(1)
-wd.find_element(By.XPATH, paths['search_button']).click()
+wd.find_element(By.XPATH, traverse_paths['search_button']).click()
 sleep(5)
 
+# testing for transactions page
+transaction_paths = cl_selectors.transaction_paths()
+issue_date = wd.find_element(By.XPATH, transaction_paths['issue_date']).text
+print(issue_date)
+
+transactions = pd.DataFrame()
+test_table = cl_transactions.scrape_transaction(wd, transactions, issue_date)
+print(transactions)
 # TODO: Eva's code here
 
-wd.find_element(By.XPATH, paths['holdings']).click()
-sleep(5)
+# wd.find_element(By.XPATH, paths['holdings']).click()
+# sleep(5)
 
 # TODO: Christina's code here
