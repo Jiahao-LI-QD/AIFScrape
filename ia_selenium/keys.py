8230f1349ef12c04cf867309ff2128eeabaecd44
Forrest <forrest0407@gmail.com>
2024-02-08 16:26:39 -0500

    Added CL Functions and updated test file.

--- ia_selenium/keys.py
+++ ia_selenium/keys.py
@@ -2,6 +2,19 @@ import sys
 import os
 
 def ia_account():
+    """
+    reads a configuration file named ia_conf and extracts the account information, web URL, and file path for CSV files.
+    :return: a dictionary containing the IA account information, web URL, and file path for CSV files.
+
+    workflow:
+    # 1. Open the ia_conf file using open and os.path.join.
+    # 2. Read lines and create key-value pairs.
+    # 3. Convert the list to a dictionary, removing whitespaces.
+    # 4. Ensure the dictionary contains “username” and “password”.
+    # 5. Verify the presence of “web_url” and “csv_path”.
+    # 6. Return the dictionary if all keys are present.
+    """
+
     with open(os.path.join(sys.path[1], r"confs\ia_conf")) as f:
         l = [line.rstrip('\n').split("=", 2) for line in f.readlines()]
         d = {key.strip(): value.strip() for key, value in l}
--- ia_selenium/split_excel.py
+++ ia_selenium/split_excel.py
@@ -1,13 +1,30 @@
 import os
-
 import pandas as pd
 
 from dbutilities import dbColumns
 
 
-# function for loading excels into pd dataframe and splitting them into equal parts of parameter value.
-# file_path is the read_excel file path, folder_path is the folder we save the excels into.
 def split_excel(file_path, folder_path, num_chunks):
+    """
+    defines a function named split_excel that takes in a file path, a folder path, and the number of chunks as inputs.
+    The function reads an Excel file, drops the first two rows, renames the columns, removes duplicate rows based on a
+    specific column, and then splits the remaining data into smaller chunks. The function saves each chunk as a separate
+    Excel file in the specified folder path and returns a list of the file paths.
+
+    :param file_path: The path of the Excel file to be split loaded from ia_conf.
+    :param folder_path: The path of the folder where the split Excel files will be saved,
+                        default is the generated folder.
+    :param num_chunks: The number of smaller chunks to split the data into, defined by input parameters in ia_app.py.
+    :return: file_paths (list): A list of file paths where the split Excel files are saved.
+
+    Workflow:
+    1. Read Excel File
+    2. Remove header rows and rename columns to fit db.
+    3. Remove duplicates contract numbers.
+    4. Split into smaller parts.
+    5. Calculate chunk size.
+    6. Save as separate Excel files.
+    """
     df = pd.read_excel(file_path).iloc[2:]
     df.columns = dbColumns.contract_columns
     df = df.drop_duplicates(subset=['Contract_number'])
@@ -16,6 +33,7 @@ def split_excel(file_path, folder_path, num_chunks):
     remainder_row = num_rows % num_chunks
     parts = []
     file_paths = []
+
     # separate the big dataframe into num_chunks amount of smaller dfs
     for i in range(num_chunks):
         start_index = i * parts_size
