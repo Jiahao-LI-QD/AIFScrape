b1a14ff494f7e2a62f4bb0c819ea2eeea9cbc9e4
jiahao-LI-QD <jiahaoli2017@gmail.com>
2024-02-15 12:27:21 -0500

    24/2/15 update 1. comment for thread_generator.py and search_traverse.py

--- utilities/thread_generator.py
+++ utilities/thread_generator.py
@@ -3,6 +3,19 @@ from utilities.tables_utilities import create_table
 
 
 def thread_generator(confs, thread_name, company, contract_file):
+    """
+    The thread_generator function is responsible for generating threads to scrape data from different companies. It
+    creates dataframes for different tables and retrieves contract numbers for the specified company. It then
+    iterates through the contract numbers, calling the scrape_traverse function to scrape data for each contract. The
+    iteration continues until there are no exceptions or until the maximum number of iterations is reached.
+
+    :param confs: Configuration dictionary
+    :param thread_name: the name of this thread
+    :param company: company name
+    :param contract_file: the contract file used for traverse loop
+
+    :return: Nothing returned
+    """
     # start the ia company scrapy process
 
     # create dataframes for all the tables
@@ -16,5 +29,6 @@ def thread_generator(confs, thread_name, company, contract_file):
             break
         iteration_time += 1
 
+    # keep only contracts that doesn't have contracts
     tables['contracts'] = tables['contracts'][~tables['contracts']['Contract_number'].isin(tables['recover'])]
     confs['threading_tables'][thread_name] = tables
