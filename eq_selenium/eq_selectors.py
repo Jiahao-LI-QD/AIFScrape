23bc7a98eb43fb05013b188594eb4bc4610dc689
jiahao-LI-QD <jiahaoli2017@gmail.com>
2024-02-20 16:43:27 -0500

    24/2/20 update 1. get account: now doesn't check web_url 2. get confs: new case 'EQ' 3. eq_scrap: login 4. cl_policies.py: file name to 'CL'

--- eq_selenium/eq_selectors.py
+++ eq_selenium/eq_selectors.py
@@ -3,7 +3,20 @@ def login_paths():
         'continue_button': '//html/body/main/section/div/div/div/div/div/form/div[2]/button',
         'username': '//*[@id="username"]',
         'password': '//*[@id="password"]',
-        'login_button': '/html/body/main/section/div/div/div/form/div[2]/button'
+        'login_button': '/html/body/main/section/div/div/div/form/div[2]/button',
+        'main_page': '//*[@id="mainnav"]/ul/li[1]/a',
+    }
+
+def policy_paths():
+    return {
+        'magnifier_button': '/html/body/main/div[6]/div/div[1]/div/div/div/app-policyinquiry/app-dashboard/app-search/div/div/div/form/div[2]/div[4]/div/button',
+        'code_table_rows': 'html/body/main/div[6]/div/div[1]/div/div/div/app-policyinquiry/simple-modal-holder/simple-modal-wrapper/div/app-organization-lookup/div/div/div[1]/div/div[4]/div/ul/li/span',
+        'next_advisor_page': '/html/body/main/div[6]/div/div[1]/div/div/div/app-policyinquiry/simple-modal-holder/simple-modal-wrapper/div/app-organization-lookup/div/div/div[1]/div/div[5]/div[2]/app-pagination/nav/ul/li',
+        'close_advisor_page': '/html/body/main/div[6]/div/div[1]/div/div/div/app-policyinquiry/simple-modal-holder/simple-modal-wrapper/div/app-organization-lookup/div/div/div[2]/button',
+        'reset_button': '/html/body/main/div[6]/div/div[1]/div/div/div/app-policyinquiry/app-dashboard/app-search/div/div/div/form/div[3]/button[2]',
+        'search_button': '/html/body/main/div[6]/div/div[1]/div/div/div/app-policyinquiry/app-dashboard/app-search/div/div/div/form/div[3]/button[1]',
+        'code_input': '/html/body/main/div[6]/div/div[1]/div/div/div/app-policyinquiry/app-dashboard/app-search/div/div/div/form/div[2]/div[3]/div/input',
+        'policy_table_rows': '/html/body/main/div[6]/div/div[1]/div/div/div/app-policyinquiry/app-dashboard/app-results/div/div/div[2]/div/div/ngx-datatable/table/tbody/tr'
     }
 
 
