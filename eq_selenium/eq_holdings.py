5515190ca0eba42dd530c9d573d7febec6542577
ViolaSy <violasong0314@gmail.com>
2024-02-24 19:34:16 -0500

    tested eq_holdings.py (2.24)

--- eq_selenium/eq_holdings.py
+++ eq_selenium/eq_holdings.py
@@ -14,9 +14,7 @@ from utilities.web_driver import driver_setup
 from utilities import get_account
 
 # scraping for investment
-confs = get_confs(companies['EQ'])
-wd = login(confs)
-wd.get(confs['parameters']['index_url'] + '600468391')
+
 paths = eq_selectors.holdings_paths()
 statement_date = datetime.today().strftime('%Y-%m-%d')
 text = wd.find_element(By.XPATH, paths['text']).text.split(' (', 1)
@@ -32,39 +30,15 @@ raw = [data[i:i + 6][:2] + [data[i + 3]] + [data[i:i + 6][-2].replace('$', '')] 
 
 if raw == []:
     result.append('TERMINATED')
-    raw = [[None]*5]
-
+    raw = [None] * 5
 else:
     result.extend([None])
 
 final_result = []
-print(raw)
 for raw_list in raw:
     final_result.append(result + raw_list + [None, 'EQ'])
 
 df = pd.DataFrame(final_result, columns=dbColumns.fund_columns)
-print(df)
-
-# check if it is a table
-# if elements[1].get_attribute('class') == "sm-show no-print":
-#     result.extend([None])
-#     for element in elements:
-# #         details_block = element.find_elements(By.XPATH, './div[2]/*')
-# #         for row in details_block:
-# #             data = row.find_elements(By.XPATH, './*')
-# #             if len(data) > 1 and data[0].tag_name == 'p':
-# #                 for item in data:
-# #                     print("Value:", item.text)
-# #             # for item in data:
-# #             #     if len(data) > 1 and data[0].tag_name == 'p':
-# #             #         fund_name = item.text
-# #             #         print(fund_name)
 #
-#
-#
-#
-# else:
-#     category = 'TERMINATED'
-#     result.append(category)
-
-# print(result)
+# holdings = pd.concat([holdings,df], ignore_index=True)
+# return holdings
--- eq_selenium/eq_selectors.py
+++ eq_selenium/eq_selectors.py
@@ -7,6 +7,7 @@ def login_paths():
         'main_page': '//*[@id="mainnav"]/ul/li[1]/a',
     }
 
+
 def policy_paths():
     return {
         'search_button': '/html/body/main/div[6]/div/div[1]/div/div/div/app-policyinquiry/app-dashboard/app-search/div/div/div/form/div[3]/button[1]',
@@ -15,12 +16,21 @@ def policy_paths():
 
 
 def client_paths():
-    return{
-        'contract_no':'/html/body/main/div[6]/div/div[1]/div/div/div/app-policyinquiry/app-dashboard/app-results/div/div/div[2]/div/div/ngx-datatable/table/tbody/tr/td[4]/a[1]',
-        'birthday':'/html/body/main/div[6]/div/div[1]/div/div/div/app-policyinquiry/app-dashboard/app-results/div/div/div[2]/div/div/ngx-datatable/table/tbody/tr/td[3]',
-        'client_tb':{
-             'client_main':'//*[@id="policy_details"]/div[3]/div/div[2]',
-            'client_row':'.//*'
+    return {
+        'contract_no': '/html/body/main/div[6]/div/div[1]/div/div/div/app-policyinquiry/app-dashboard/app-results/div/div/div[2]/div/div/ngx-datatable/table/tbody/tr/td[4]/a[1]',
+        'birthday': '/html/body/main/div[6]/div/div[1]/div/div/div/app-policyinquiry/app-dashboard/app-results/div/div/div[2]/div/div/ngx-datatable/table/tbody/tr/td[3]',
+        'client_tb': {
+            'client_main': '//*[@id="policy_details"]/div[3]/div/div[2]',
+            'client_row': './/*'
 
         }
+    }
+
+
+def holdings_paths():
+    return {
+        'text': '//*[@id="policy_content"]/div[1]/h1[1]',
+        'account_type': '//*[@id="policy_content"]/div[3]/div[1]/p[2]/span',
+        'table_data': '//*[@id="policy_details"]/div[3]/div/div[3]/div[1]/table/tbody/tr/td'
+
     }
