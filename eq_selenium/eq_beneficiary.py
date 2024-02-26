9e39d339bac82845b8b63f414b4567e85286cbcd
Quyy2023 <Eva.Qu2023@gmail.com>
2024-02-26 17:49:06 -0500

    scrape client participant beneficiary

--- eq_selenium/eq_beneficiary.py
+++ eq_selenium/eq_beneficiary.py
@@ -13,33 +13,25 @@ def scrape_beneficiary(wd,beneficiary):
     paths = eq_selectors.beneficiary_paths()
     header = wd.find_element(By.XPATH, '//*[@id="policy_content"]/div[1]/h1[1]').text
     contract_number =re.findall(r'\((.*?)\)',header)
-    # print(contract_number)
+    print(contract_number)
     wd.find_element(By.XPATH, paths['beneficiary']).click()
     b_table = wd.find_elements(By.XPATH, paths['b_table']['b_main'])
 
     for b3_row in b_table:
         b3 = [b3.text for b3 in b3_row.find_elements(By.XPATH, paths['b_table']['b_row'])]
-        print(len(b3))
-        print(b3)
-        print(b3[-1])
+        # print(b3)
         for b in b3:
-            print(b)
+            # print(b)
             if b=='':
                 continue
             bs = b.split('\n')
-            print(bs)
+            # print(bs)
             if len(bs)%2==0:
                 b_item = [bi for index, bi in enumerate(bs) if index % 2 == 0]
             else:
                 b_item=[bs[0],bs[3],bs[5]]
-
-            # print(b_item[0])
-            # print(b_item[-1])
-            # print(b_item[-2])
-            print(b_item)
-            print(len(b_item))
-            if len(b_item) > 1:
-                result = [contract_number[0], None, b_item[1], b_item[0], float(b_item[-1].strip('%'))/100, None, None, None, companies['EQ']]
+            if len(b_item) > 3:
+                result = [contract_number[0], None, b_item[-2], b_item[0], float(b_item[-1].strip('%'))/100, None, None, None, companies['EQ']]
             else:
                 result = [contract_number[0], None, None, b_item[0], None, None, None, None, companies['EQ']]
-            return result
+            print(result)
