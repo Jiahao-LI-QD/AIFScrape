1237079a1c627a60848089df5998d45dd8fafb23
ViolaSy <violasong0314@gmail.com>
2024-02-26 14:55:31 -0500

    final draft eq_holdings(tested)

--- eq_selenium/eq_holdings.py
+++ eq_selenium/eq_holdings.py
@@ -14,11 +14,6 @@ from utilities.get_confs import get_confs
 from utilities.web_driver import driver_setup
 from utilities import get_account
 
-# scraping for investment
-# confs = get_confs(companies['EQ'])
-# wd = login(confs)
-# wd.get(confs['parameters']['index_url'] + '600468391')
-
 
 def scrape_holdings(wd, holdings):
     paths = eq_selectors.holdings_paths()
@@ -35,8 +30,8 @@ def scrape_holdings(wd, holdings):
 
     row = wd.find_elements(By.XPATH, paths['table_data'])
     data = [data.text for data in row]
-    raw = [data[i:i + 6][:2] + [data[i + 3]] + [data[i:i + 6][-2].replace('$', '')] + data[i:i + 6][-1:] for i in
-           range(0, len(data), 6)]
+    raw = [[data[i + 1], data[i]] + [data[i + 3]] + [data[i:i + 6][-2].replace('$', '')]
+           + data[i:i + 6][-1:] for i in range(0, len(data), 6)]
 
     if raw == []:
         result.append('TERMINATED')
@@ -50,8 +45,3 @@ def scrape_holdings(wd, holdings):
         final_result.append(result + raw_list + [None, 'EQ'])
 
     return final_result
-
-# df = pd.DataFrame(final_result, columns=dbColumns.fund_columns)
-# print(df)
-# holdings = pd.concat([holdings,df], ignore_index=True)
-# return holdings
