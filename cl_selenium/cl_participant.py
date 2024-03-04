8d4376d9f6ca8dffcab36099e10f3aada454755f
AIF-Data <Eva.Qu2023@gmail.com>
2024-03-04 14:08:39 -0500

    update the beneficiary and client participant

--- cl_selenium/cl_participant.py
+++ cl_selenium/cl_participant.py
@@ -25,12 +25,19 @@ def scrape_participant(wd,participant):
     paths = cl_selectors.participant_paths()
     contract_number = wd.find_element(By.XPATH, paths['contract_number']).text
     # print(contract_number)
-    wd.find_element(By.XPATH, paths['participant_hide']).click()
     p_item = wd.find_elements(By.XPATH, paths['participant_table']['participant_main'])
     for p_row in p_item:
         row = [p.text.split('\n', 1)[0] for p in
                p_row.find_elements(By.XPATH, paths['participant_table']['participant_row'])]
-        result = [contract_number, row[0], row[1], row[-1], companies['CL']]
-        # print(result)
+        if len(row)==0:
+            result=[[None]*3]
+        else:
+            wd.find_element(By.XPATH, paths['participant_hide']).click()
+            p_item = wd.find_elements(By.XPATH, paths['participant_table']['participant_main'])
+            for p_row in p_item:
+                row = [p.text.split('\n', 1)[0] for p in
+                            p_row.find_elements(By.XPATH, paths['participant_table']['participant_row'])]
+            result = [contract_number, row[0], row[1], row[-1], companies['CL']]
+        print(result)
         participant.loc[len(participant)]=result
-    return participant
+        print(participant)
